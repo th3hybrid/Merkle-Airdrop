b40b9c5a78c9f29742f746952c8823c8ccc8b5a4
th3hybrid <emmanueloludare928@gmail.com>
2024-11-26 20:37:19 +0100

    first commit

---
+++ test/MerkleAirdropTest.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {DeployMerkleAirdrop} from "../../script/DeployMerkleAirdrop.s.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public merkleRoot =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [proofOne, proofTwo];
+    uint256 public amountToClaim = 25 * 1e18;
+    uint256 public amountToSend = amountToClaim * 4;
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        if (!isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
+        } else {
+            token = new BagelToken();
+            airdrop = new MerkleAirdrop(merkleRoot, token);
+            token.mint(token.owner(), amountToSend);
+            token.transfer(address(airdrop), amountToSend);
+        }
+        //gasPayer = makeAddr("gasPayer");
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, amountToClaim, proof);
+        uint256 endingBalance = token.balanceOf(user);
+
+        console.log(endingBalance);
+        assertEq(endingBalance - startingBalance, amountToClaim);
+    }
+}
